146af5563676b742389e0ffd41f83b43a20c2f34
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2022-04-14 18:42:47 +0200

    for promobile users

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const isValidMobilePhone = phone => {
-  const validMobilePhoneRegEx = /((\+221|00221)?)((7[7608][0-9]{7}$)|(3[03][98][0-9]{6}$))/;
+  const validMobilePhoneRegEx = /((\+221|00221)?)((7[76085][0-9]{7}$)|(3[03][98][0-9]{6}$))/;
   return validMobilePhoneRegEx.test(phone);
 }
 
